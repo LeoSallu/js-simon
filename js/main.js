1699df9dc8be632582fdcac26c4252184c174b28
LeoSallu <L.sallustio99@gmail.com>
2023-03-06 15:35:21 +0100

    Impostazione funzione e ciclo for

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Funzioni 
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+//function myCreateElement(htmltag, content)
+
+
+
+
+
+
+// Main
+
+const numBox= document.querySelector('.list');
+
+for(let i=0; i<5; i++){
+    let number = getRandomInt(1, 50);
+    console.log(number);
+}
